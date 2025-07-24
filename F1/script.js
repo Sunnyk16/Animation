a0bffb992cb9d3d7ee35b900cbc0cc9a3b6a9ce0
Sunnyk16 <kumarsunny1246@gmail.com>
2025-07-24 15:26:45 +0530

    refactor: restructure HTML layout and update GSAP animations for navigation

--- F1/script.js
+++ F1/script.js
@@ -1,6 +1,18 @@
-gsap.to("#box",{
-    x: 500,
-    duration: 2,
-    delay: 1,
+var t1 = gsap.timeline();
 
+t1.to("#nav",{
+
+})
+
+t1.from("h2",{
+    duration: 1,
+    opacity: 0,
+    y: -50, 
+    delay: 0.5,
+})
+t1.from("h4",{
+    duration: 1,
+    opacity: 0,
+    y: -50,
+    stagger:0.3,
 })
